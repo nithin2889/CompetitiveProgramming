e201813f7df7a9bf845c6e866a56412f69df4028
Nithin Prasad <nithin2889@gmail.com>
2021-03-16 22:41:41 +0530

    Power generation using binary exponentiation and modular exponentiation

--- src/main/java/cp/binaryexponentiation/PowerGeneration.java
+++ src/main/java/cp/binaryexponentiation/PowerGeneration.java
@@ -3,8 +3,11 @@ package cp.binaryexponentiation;
 public class PowerGeneration {
 
   public static void main(String[] args) {
-    long base = 3, power = 4;
+    long base = 3, power = 100;
     System.out.println("Power(" + base + '^' + power + ") is: " + power(base, power));
+
+    double b = 3, pow = 100;
+    System.out.println("ModPower(" + base + '^' + power + ") is: " + modPower(b, pow));
   }
 
   private static long power(long base, long power) {
@@ -16,6 +19,22 @@ public class PowerGeneration {
         power /= 2;
       } else {
         ans = ans * base;
+        power = power - 1;
+      }
+    }
+    return ans;
+  }
+
+  private static double modPower(double base, double power) {
+    double ans = 1.0;
+    double mod = 1e9 + 7;
+
+    while (power > 0) {
+      if (power % 2 == 0) {
+        base = (base * base) % mod;
+        power /= 2;
+      } else {
+        ans = (ans * base) % mod;
         power = power - 1;
       }
     }

1cdc3011ea065b6cabcb63bc3dd3f034c5b1e38b
Nithin Prasad <nithin2889@gmail.com>
2021-03-16 22:10:13 +0530

    Power generation using binary exponentiation

--- src/main/java/cp/binaryexponentiation/PowerGeneration.java
+++ src/main/java/cp/binaryexponentiation/PowerGeneration.java
@@ -3,19 +3,18 @@ package cp.binaryexponentiation;
 public class PowerGeneration {
 
   public static void main(String[] args) {
-    long base = 10, power = 10;
+    long base = 3, power = 4;
     System.out.println("Power(" + base + '^' + power + ") is: " + power(base, power));
   }
 
   private static long power(long base, long power) {
     long ans = 1;
+
     while (power > 0) {
-      // for even powers
       if (power % 2 == 0) {
         base = base * base;
-        power = power / 2;
+        power /= 2;
       } else {
-        // for odd powers
         ans = ans * base;
         power = power - 1;
       }

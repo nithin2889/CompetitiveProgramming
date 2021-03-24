01e0b3a1933cc838d73307b97d470ac8d282bc0d
Nithin Prasad <nithin2889@gmail.com>
2021-03-24 20:22:55 +0530

    CP - counting the number of set bits in a number

---
+++ src/main/java/cp/bitmanipulation/CountOfSetBits.java
@@ -0,0 +1,18 @@
+package cp.bitmanipulation;
+
+public class CountOfSetBits {
+
+  public static void main(String[] args) {
+    int num = 13;
+    System.out.println("Number of set bits in " + num + " are: " + countSetBits(num));
+  }
+
+  private static int countSetBits(int num) {
+    int count = 0;
+    while (num > 0) {
+      count += num & 1;
+      num = num >> 1;
+    }
+    return count;
+  }
+}

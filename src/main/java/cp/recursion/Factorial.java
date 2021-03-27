b24919b5ef2be140ee6565e232178f5454a66391
Nithin Prasad <nithin2889@gmail.com>
2021-03-27 19:43:21 +0530

    CP - factorial using recursion

---
+++ src/main/java/cp/recursion/Factorial.java
@@ -0,0 +1,17 @@
+package cp.recursion;
+
+public class Factorial {
+
+  public static void main(String[] args) {
+    int n = 5;
+    System.out.println("Factorial of " + n + " is: " + fact(n));
+  }
+
+  private static int fact(int n) {
+    if (n == 1) {
+      return 1;
+    }
+
+    return n * fact(n - 1);
+  }
+}

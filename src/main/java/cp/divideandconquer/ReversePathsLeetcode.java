c11256b1c79ca909f4fdb7553cd42b3f9ae6801d
Nithin Prasad <nithin2889@gmail.com>
2021-04-03 17:57:12 +0530

    CP - Count number of reverse pairs leetcode

---
+++ src/main/java/cp/divideandconquer/ReversePathsLeetcode.java
@@ -0,0 +1,53 @@
+package cp.divideandconquer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReversePathsLeetcode {
+
+  public static void main(String[] args) {
+    int[] arr = {6, 8, 10, 12, 2, 2, 3, 3, 5};
+    int n = arr.length;
+
+    System.out.println("Reverse pairs count is: " + solve(0, n - 1, arr));
+  }
+
+  private static int solve(int low, int high, int[] arr) {
+    if (low == high) {
+      return 0;
+    }
+    int mid = (low + high) / 2;
+
+    int left = solve(low, mid, arr);
+    int right = solve(mid + 1, high, arr);
+
+    return merge(low, mid, high, arr) + left + right;
+  }
+
+  private static int merge(int low, int mid, int high, int[] arr) {
+    int left = 0;
+    int right = mid + 1;
+    int count = 0;
+
+    List<Integer> temp = new ArrayList<>();
+
+    for (int i = low; i <= mid; i++) {
+      while (arr[low] > 2 * arr[right] && right < high) {
+        low++;
+        right++;
+      }
+      count += (right - (mid + 1));
+    }
+
+    while (left <= mid) {
+      temp.add(arr[left]);
+      left++;
+    }
+
+    while (right <= high) {
+      temp.add(arr[right]);
+      right++;
+    }
+    return count;
+  }
+}
